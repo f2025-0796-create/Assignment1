6e3e6311226c234c99659b36c9c408fd9f1a6546
f2025-0796-create <f2025-0796@bnu.edu.pk>
2025-10-17 09:45:33 +0500

    Add files via upload

---
+++ bonus.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+ using namespace std;
+ int main() {
+int a,b;
+cout<<“enter two numbers”<< endl;
+cin>>a;
+cin>> b;
+cout<< “ values before swap “;
+a= a+b;
+b= a-b;
+a=a-b;
+cout<<“ values after swap”;
+cout<<“a”<<a<< endl;
+cout<<“b”<< b<<;
+return 0;
+}
+
---
+++ q2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main() {
+double   marks,total,marks1, marks2, marks3, marks4, marks5;
+double per ,obt ;
+char grades;
+cout<<“enter marks1”<<endl;
+cin>>marks1;
+cout<<“enter marks2”<<endl;
+cin>> marks2;
+cout<<“ enter marks3”<<endl; 
+cin>> marks3;
+cout<<“ enter marks4”<<endl;
+cin>> marks4;
+cout<<“enter marks5”<<endl;
+cin>> marks5; 
+ per= (obt/ total)*100;
+if( obt marks>90){
+cout<<“you got A+”<<grades;}
+if(obt marks>80){
+cout<<“ you got A “<<grades;}
+if(obt marks>70){
+cout<<“you got B”<<grades;} 
+if ( obt marks> 60){
+ cout<<“ you got c”<<grades;}
+if( obt marks<60){ 
+cout<<“ you are fail try again”<< grade;}
+cout <<“your percentage is”<<per; 
+return 0;
+}
+
